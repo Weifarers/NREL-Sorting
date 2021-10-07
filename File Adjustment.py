e102cd996af6e04321f822dd64d0a3d857eacf43
Wei Trinh <wei.trinh@gmail.com>
2021-10-06 22:06:30 -0500

    Update File Adjustment.py
    Adding an unzipping function, and more cleanly storing the data for use later.

--- File Adjustment.py
+++ File Adjustment.py
@@ -1,50 +1,78 @@
 import os
 import shutil
+import zipfile
 
 
 def main():
+    # unzip()
+    data_filter()
+
+
+def unzip():
+    # Writing this function to unzip all the files for me.
     # Generates the parent path. The architecture for this code is based on the idea that all of the code that
     # handles the data is kept in a separate folder, named 'Code'. The parent path contains all the folders with
     # our data of interest.
     parent_path = os.path.dirname(os.getcwd())
+    # Since the ZIP files are always stored in the same place, I'll just hard code in their location.
+    zip_dir = parent_path + '\\ZIP Files'
 
-    # Gets a list of all the folders in the parent path, each folder of which contains our data.
-    parent_list = os.listdir(parent_path)
+    # Gets all the files that are in the ZIP folder.
+    file_names = [f for f in os.listdir(zip_dir) if os.path.isfile(os.path.join(zip_dir, f))]
+
+    # Creates a data folder that we'll store everything in.
+    os.mkdir(parent_path + '\\Data')
+
+    # Goes through every ZIP file one by one.
+    for i in range(len(file_names)):
+        # Grabs the current ZIP file.
+        current_zip = zip_dir + '\\' + file_names[i]
+        # Extracts it to our parent path.
+        with zipfile.ZipFile(current_zip, "r") as zip_ref:
+            zip_ref.extractall(parent_path + '\\Data')
+
+
+def data_filter():
+    # Generates the parent path. The architecture for this code is based on the idea that all of the code that
+    # handles the data is kept in a separate folder, named 'Code'. The parent path contains all the folders with
+    # our data of interest.
+    parent_path = os.path.dirname(os.getcwd()) + '\\Data'
+
+    # Gets a list of all the folders in the parent path, each folder of which contains our raw data.
+    parent_list = os.listdir(parent_path + '\\Raw Data')
 
     # Establishes a location to store all the data we want.
-    store_loc = parent_path + '\\Actual Data'
+    store_dir = parent_path + '\\Filtered Data'
+    try:
+        os.mkdir(store_dir)
+    except OSError:
+        # If the directory already exists, don't complain.
+        pass
 
     # This sets our filter for terms we don't want.
     remove_words = ['DA', 'HA4', 'DPV']
 
-    # Sets any folders we want to skip over. 
-    skip_folders = ['Code', 'ZIP Files']
-
     # We'll iterate through every folder in there.
     for i in range(len(parent_list)):
-        # Skips over folders we don't want to mess with.
-        if parent_list[i] in skip_folders:
-            continue
-        else:
-            # Gets the current folder we're looking at.
-            curr_dir = parent_path + '\\' + parent_list[i]
+        # Gets the current folder we're looking at.
+        curr_dir = parent_path + '\\Raw Data\\' + parent_list[i]
 
-            try:
-                # Gets all the file names that are in that current folder.
-                file_names = [f for f in os.listdir(curr_dir)
-                              if os.path.isfile(os.path.join(curr_dir, f))]
-                # Iteratively checks every file.
-                for j in range(len(file_names)):
-                    # All the files are split by an underscore between each word. If the list contains any of
-                    # the words we want removed, then delete it.
-                    if len([x for x in file_names[j].split('_') if x in remove_words]) != 0:
-                        os.remove(curr_dir + '\\' + file_names[j])
-                    # Otherwise, move it to a new location where we consolidate all of the data.
-                    else:
-                        shutil.move(curr_dir + '\\' + file_names[j], store_loc + '\\' + file_names[j])
-            # Skips any files that are not a directory (zip files, etc).
-            except NotADirectoryError:
-                continue
+        try:
+            # Gets all the file names that are in that current folder.
+            file_names = [f for f in os.listdir(curr_dir)
+                          if os.path.isfile(os.path.join(curr_dir, f))]
+            # Iteratively checks every file.
+            for j in range(len(file_names)):
+                # All the files are split by an underscore between each word. If the list contains any of
+                # the words we want removed, then skip it.
+                if len([x for x in file_names[j].split('_') if x in remove_words]) != 0:
+                    continue
+                # Otherwise, move it to a new location where we consolidate all of the data.
+                else:
+                    shutil.move(curr_dir + '\\' + file_names[j], store_dir + '\\' + file_names[j])
+        # Skips any files that are not a directory (zip files, etc).
+        except NotADirectoryError:
+            continue
 
 
 if __name__ == "__main__":
